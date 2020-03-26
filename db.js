e46872865da10ff9c844f92c91811199d5ff540a
Florian BORIE <florian.borie@ynov.com>
2020-03-26 17:17:28 +0100

    corection de bug

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const donnee = require('express').Router()
 
 db.open('api.db').then(() => {
     Promise.all([
-        db.run("CREATE TABLE IF NOT EXISTS Formations (id INTEGER PRIMARY KEY AUTOINCREMENT, annee Date, formation varchar(255), ville varchar(255))"),
+      db.run("CREATE TABLE IF NOT EXISTS Formations (id INTEGER PRIMARY KEY AUTOINCREMENT, annee Date, formation varchar(255), ville varchar(255))"),
     ]).then(() => {
       console.log('Databases are ready')
     }).catch((err) => {
--- models/formations.js
+++ models/formations.js
@@ -4,11 +4,14 @@ module.exports = {
   getAllFormation() {
     return db.all("SELECT rowid AS id, * FROM Formations")
   },
+  findOneFormation(id) {
+    return db.get("SELECT rowid AS id, * FROM Formations WHERE rowid = ?", id)
+  },
   async insertFormation(body) { 
     const { lastID } = await db.run("INSERT INTO Formations (annee, formation, ville) VALUES(?, ?, ?)", [body.annee, body.formation, body.ville])
 
     return this.findOneFormation(lastID)   
-},
+  },
   deleteFormation(id) {
     return db.run("DELETE FROM Formations WHERE rowid = ?", id)
   },
